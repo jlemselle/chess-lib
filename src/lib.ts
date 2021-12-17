75ff2acbf436b44efc938f67225a4ae3390bb812
James Emselle <james.emselle@outlook.com>
2021-12-18 10:23:35 +1100

    Moved to NPM instead of Yarn, added basic position parsing

--- src/lib.ts
+++ src/lib.ts
@@ -2,4 +2,55 @@ export enum Player {
     WHITE, BLACK
 }
 
+export enum PieceType {
+    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+}
+
+export interface Piece {
+    type: PieceType,
+    player: Player
+}
+
 export const firstMove = Player.WHITE;
+
+export const startingPosition = pos(`
+    rnbqkbnr
+    pppppppp
+    ........
+    ........
+    ........
+    ........
+    PPPPPPPP
+    RNBQKBNR
+`);
+
+/**
+ * Create a chess board from a plain string.
+ * @param raw 
+ */
+export function pos(raw: string): (Piece | undefined)[] {
+    const whitespaceRemoved = raw.replace(/\s/gm, '');
+    return Array(...whitespaceRemoved).map(c => charToPiece(c));
+}
+
+function charToPiece(char: string): Piece | undefined {
+    const type =
+        ['P', 'p'].includes(char) ? PieceType.PAWN
+            : ['N', 'n'].includes(char) ? PieceType.KNIGHT
+                : ['B', 'b'].includes(char) ? PieceType.BISHOP
+                    : ['R', 'r'].includes(char) ? PieceType.ROOK
+                        : ['Q', 'q'].includes(char) ? PieceType.QUEEN
+                            : ['K', 'k'].includes(char) ? PieceType.KING
+                                : undefined
+
+    const player =
+        ['P', 'N', 'B', 'R', 'Q', 'K'].includes(char) ? Player.WHITE
+            : ['p', 'n', 'b', 'r', 'q', 'k'].includes(char) ? Player.BLACK
+                : undefined;
+
+    if (type !== undefined && player !== undefined) {
+        return { type, player }
+    }
+
+    return undefined;
+}
--- test/first-move.ts
+++ test/first-move.ts
@@ -1,5 +1,22 @@
-import { firstMove, Player } from "../src/lib"
+import { firstMove, Player, pos, startingPosition } from "../src/lib"
 
 test('first move should be white', () => {
     expect(firstMove).toBe(Player.WHITE)
+})
+
+test('starting position should be 64 squares', () => {
+    expect(startingPosition.length).toBe(64);
+})
+
+test('should have starting position', () => {
+    expect(startingPosition).toEqual(pos(`
+        rnbqkbnr
+        pppppppp
+        ........
+        ........
+        ........
+        ........
+        PPPPPPPP
+        RNBQKBNR
+    `));
 })

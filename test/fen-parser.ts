2b50c6c8334ee6e23aa07b47bf8385b253cb42e8
James Emselle <james.emselle@outlook.com>
2021-12-27 10:14:15 +1100

    Started Piece parsing

--- test/fen-parser.ts
+++ test/fen-parser.ts
@@ -1,4 +1,41 @@
-import { a6, a7, a8, e3, f6, h8, squareMap } from "../src/squares";
+import {
+  a1,
+  a2,
+  a6,
+  a7,
+  a8,
+  b1,
+  b2,
+  b7,
+  b8,
+  c1,
+  c2,
+  c7,
+  c8,
+  d1,
+  d2,
+  d7,
+  d8,
+  e1,
+  e2,
+  e3,
+  e7,
+  e8,
+  f1,
+  f2,
+  f6,
+  f7,
+  f8,
+  g1,
+  g2,
+  g7,
+  g8,
+  h1,
+  h2,
+  h7,
+  h8,
+  squareMap,
+} from "../src/squares";
 
 interface PiecePlacement {
   pawn: number;
@@ -41,12 +78,12 @@ class FenParser {
     return {
       white: {
         piecePlacement: {
-          pawn: 0,
-          knight: 0,
-          bishop: 0,
-          rook: 0,
-          queen: 0,
-          king: 0,
+          pawn: this.parsePiece(piecePlacement, "P"),
+          knight: this.parsePiece(piecePlacement, "N"),
+          bishop: this.parsePiece(piecePlacement, "B"),
+          rook: this.parsePiece(piecePlacement, "R"),
+          queen: this.parsePiece(piecePlacement, "Q"),
+          king: this.parsePiece(piecePlacement, "K"),
         },
         castling: {
           kingside: castling.includes("K"),
@@ -55,12 +92,12 @@ class FenParser {
       },
       black: {
         piecePlacement: {
-          pawn: 0,
-          knight: 0,
+          pawn: this.parsePiece(piecePlacement, "p"),
+          knight: this.parsePiece(piecePlacement, "n"),
           bishop: this.parsePiece(piecePlacement, "b"),
           rook: this.parsePiece(piecePlacement, "r"),
-          queen: 0,
-          king: 0,
+          queen: this.parsePiece(piecePlacement, "q"),
+          king: this.parsePiece(piecePlacement, "k"),
         },
         castling: {
           kingside: castling.includes("k"),
@@ -75,24 +112,27 @@ class FenParser {
   }
 
   private parsePiece(board: string, piece: string): number {
-    return board
+    const result = board
       .split("/")
       .map((rank, index) => {
-        return this.parseRank(rank, piece) << index;
+        const rankValue = this.parseRank(rank, piece) << (index * 8);
+        return rankValue;
       })
-      .reduce((a, b) => a | b);
+      .reduce((a, b) => i64or(a, b));
+    return result;
   }
 
   private parseRank(rank: string, piece: string): number {
-    return Array.from(this.expandRank(rank))
-      .map((square, index) => {
-        if (square === piece) {
-          return 1 << index;
-        } else {
-          return 0;
-        }
-      })
-      .reduce((a, b) => a | b);
+    const expandedRank = this.expandRank(rank);
+    const result = Array.from(expandedRank).map((square, index) => {
+      if (square === piece) {
+        return 1 << index;
+      } else {
+        return 0;
+      }
+    });
+
+    return result.reduce((a, b) => i64or(a, b));
   }
 
   private expandRank(rank: string): string {
@@ -252,7 +292,7 @@ class FenParser {
     black: {
       pawn: 0,
       knight: 0,
-      bishop: a6 | f6,
+      bishop: c8 | f8,
       rook: 0,
       queen: 0,
       king: 0,
@@ -266,11 +306,57 @@ class FenParser {
       king: 0,
     },
   },
+  {
+    fen: "8/pppppppp/8/8/8/8/8/9 w - - 0 0",
+    black: {
+      pawn: a7 | b7 | c7 | d7 | e7 | f7 | g7 | h7,
+      knight: 0,
+      bishop: 0,
+      rook: 0,
+      queen: 0,
+      king: 0,
+    },
+    white: {
+      pawn: 0,
+      knight: 0,
+      bishop: 0,
+      rook: 0,
+      queen: 0,
+      king: 0,
+    },
+  },
+  {
+    fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0",
+    black: {
+      pawn: a7 | b7 | c7 | d7 | e7 | f7 | g7 | h7,
+      knight: b8 | g8,
+      bishop: c8 | f8,
+      rook: a8 | h8,
+      queen: d8,
+      king: e8,
+    },
+    white: {
+      pawn: squares(a2, b2, c2, d2, e2, f2, g2, h2),
+      knight: squares(b1, g1),
+      bishop: squares(c1, f1),
+      rook: squares(a1, h1),
+      queen: squares(d1),
+      king: squares(e1),
+    },
+  },
 ].forEach((testCase) => {
-  test("piece placement - rook parsing", () => {
+  test("piece placement", () => {
     const parser: FenParser = new FenParser();
     const result = parser.parse(testCase.fen);
     expect(result.black.piecePlacement).toEqual(testCase.black);
     expect(result.white.piecePlacement).toEqual(testCase.white);
   });
 });
+
+function squares(...squares: number[]): number {
+  return squares.reduce((a, b) => Number(BigInt(a) | BigInt(b)));
+}
+
+function i64or(a: number, b: number): number {
+  return Number(BigInt(a) | BigInt(b));
+}
